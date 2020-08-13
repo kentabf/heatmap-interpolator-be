f9c1c59fee331c29f922490eef1c37a866ef58b7
Kenta Baron-Furuyama <kentabaronfuruyama@gmail.com>
2020-08-13 15:23:54 +0900

    heroku 2

--- src/main/scala/server/MyServer.scala
+++ src/main/scala/server/MyServer.scala
@@ -2,36 +2,65 @@ package server
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 
 object MyServer {
 
-  val testContent =
-    """
-      |<html>
-      | <head>Test</head>
-      | <body>
-      |   Test HTML content served by Akka HTTP on Heroku
-      | </body>
-      |</html>
-      |""".stripMargin
-
-  val route = get {
-    complete(
-      HttpEntity(
-        ContentTypes.`text/html(UTF-8)`,
-        testContent
-      )
-    )
-  }
-
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem("Server")
+
+    val routes = NewpageRouter.route ~ HomepageRouter.route
 
     val host = "0.0.0.0"
     val port = sys.env.getOrElse("PORT", "8080").toInt
 
-    Http().bindAndHandle(route, host, port)
+    Http().bindAndHandle(routes, host, port)
+  }
+}
+
+object NewpageRouter {
+  val content =
+    """
+      |<html>
+      | <head>Test Newpage</head>
+      | <body>
+      |   Test Newpage HTML content served by Akka HTTP on Heroku
+      | </body>
+      |</html>
+      |""".stripMargin
+
+  val route = path("new") {
+    get {
+      complete(
+        HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          content
+        )
+      )
+    }
+  }
+}
+
+object HomepageRouter {
+  val content =
+    """
+      |<html>
+      | <head>Test Homepage</head>
+      | <body>
+      |   Test Homepage HTML content served by Akka HTTP on Heroku
+      | </body>
+      |</html>
+      |""".stripMargin
+
+  val route = path("home") {
+    get {
+      complete(
+        HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          content
+        )
+      )
+    }
   }
 }
