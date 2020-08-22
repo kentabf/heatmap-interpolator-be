df058ca87db2d48ffb8c235b89c6d41cb5aa9047
Kenta Baron-Furuyama <kentabaronfuruyama@gmail.com>
2020-08-22 09:15:13 +0900

    optimized Barnes further by removing duplicate calculation of secondpass values

--- src/main/scala/interpolation/BarnesInterpolator.scala
+++ src/main/scala/interpolation/BarnesInterpolator.scala
@@ -7,31 +7,36 @@ class BarnesInterpolator(width: Int, height: Int, sample: TemperaturesData, scal
 
   override val name: String = "Barnes"
 
+  def weight(ml1: MapLocation, ml2: MapLocation, gamma: Double): Double = {
+    val dist = ml1.calcDistance(ml2)
+    if (dist > radius) {
+      0.0
+    } else {
+      exp(-pow(dist, 2)/(radius*gamma))
+    }
+  }
+
+  def firstPass(mlToInterpolate: MapLocation): Temperature = {
+    val numerAndDenum: (Double, Double) = sample
+      .map( (point: Point) => (weight(point.location, mlToInterpolate, 1.0), point.temperature))
+      .foldLeft((0,0): (Double, Double))( (acc, wat) => (acc._1 + wat._1*wat._2, acc._2 + wat._1))
+    numerAndDenum._1/numerAndDenum._2
+  }
+
+  def secondPass(mlToInterpolate: MapLocation): Temperature = {
+    val numerAndDenum: (Double, Double) = sample
+      .map( (point: Point) => (weight(point.location, mlToInterpolate, 0.3), point.temperature - firstPass(point.location)))
+      .foldLeft((0,0): (Double, Double))( (acc, wat) => (acc._1 + wat._1*wat._2, acc._2 + wat._1))
+    numerAndDenum._1/numerAndDenum._2
+  }
+
+  val firstPassSample = sample.map((point: Point) => (point, point.temperature - firstPass(point.location)))
+
   def interpolateTemperature(mlToInterpolate: MapLocation): Temperature = {
-
-    def weight(ml1: MapLocation, ml2: MapLocation, gamma: Double): Double = {
-      val dist = ml1.calcDistance(ml2)
-      if (dist > radius) {
-        0.0
-      } else {
-        exp(-pow(dist, 2)/(radius*gamma))
-      }
-    }
-
-    def firstPass(mlToInterpolate: MapLocation): Temperature = {
-      val numerAndDenum: (Double, Double) = sample
-        .map( (point: Point) => (weight(point.location, mlToInterpolate, 1.0), point.temperature))
-        .foldLeft((0,0): (Double, Double))( (acc, wat) => (acc._1 + wat._1*wat._2, acc._2 + wat._1))
-      numerAndDenum._1/numerAndDenum._2
-    }
-
-    def secondPass(mlToInterpolate: MapLocation): Temperature = {
-      val numerAndDenum: (Double, Double) = sample
-        .map( (point: Point) => (weight(point.location, mlToInterpolate, 0.3), point.temperature - firstPass(point.location)))
-        .foldLeft((0,0): (Double, Double))( (acc, wat) => (acc._1 + wat._1*wat._2, acc._2 + wat._1))
-      numerAndDenum._1/numerAndDenum._2
-    }
-
-    firstPass(mlToInterpolate) + secondPass(mlToInterpolate)
+    val firstPassVal = firstPass(mlToInterpolate)
+    val secondPassNumerandDenum = firstPassSample
+      .map{ case (point, fp) => (weight(point.location, mlToInterpolate, 0.3), fp)}
+      .foldLeft((0,0): (Double, Double))( (acc, wat) => (acc._1 + wat._1 * wat._2, acc._2 + wat._1))
+    firstPassVal - secondPassNumerandDenum._1/secondPassNumerandDenum._2
   }
 }
